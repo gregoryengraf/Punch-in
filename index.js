2fd09a2ef7e79b7beb8c55eb11863ec80fa749d2
Gregory Engraf <gregory0409@gmail.com>
2022-07-14 19:57:05 -0300

    :construction: WIP

--- index.js
+++ index.js
@@ -1,67 +1,28 @@
+require("dotenv").config();
 const express = require("express");
-const pup = require("puppeteer");
-const aws = require("aws-sdk");
+const Bree = require("bree");
 
-const user = "gregory.engraf@informa.com";
-const password = "ANAluiza12";
+// cron: '30 08 ? * MON,TUE,WED,THU,FRI',
+// cronValidate: {
+//     override: {
+//         useBlankDay: true
+//     }
+// }
+const bree = new Bree({
+    jobs: [
+        {
+            name: 'point',
+            cron: '*/1 * * * *',
+        }
+    ]
+})
 
-const s3 = new aws.S3({
-    endpoint: "https://ewr1.vultrobjects.com",
-    accessKeyId: "EWQVAEMPZ3V1MUCL48K1",
-    secretAccessKey: "aJKA7uad2FynneYfnKkDz0ubcltxmleEs9jQBhaJ",
-    region: "ewr1"
-});
-
-const checkPoint = async () => {
-    const browser = await pup.launch({headless: true});
-    const page = await browser.newPage();
-
-    console.log("Inicia");
-    const context = browser.defaultBrowserContext();
-    await context.overridePermissions("https://app2.pontomais.com.br/registrar-ponto", ['geolocation'])
-
-    await page.setGeolocation({latitude:-25.495198, longitude:-49.2415078})
-    await page.setViewport({ width: 1280, height: 650 })
-    await page.goto("https://app2.pontomais.com.br");
-
-    await page.waitForSelector('input[type=text]');
-    await page.type('input[type=text]', user);
-    await page.type('input[type=password]', password);
-
-    console.log("Preenche o login");
-    await page.waitForTimeout(500);
-
-    await Promise.all([
-        page.waitForNavigation(),
-        page.keyboard.press('Enter'),
-    ])
-
-    await page.waitForSelector('.btn-register');
-    console.log("Espera o carregamento do endereço");
-    await page.waitForTimeout(10000);
-
-    console.log("Clica no ponto");
-    // await page.mouse.click(990, 578);
-    await page.mouse.click(1009, 514);
-    await page.waitForTimeout(2000);
-
-    console.log("Gera print do ponto");
-    const dateNow = new Date().getTime();
-    const screenshot = await page.screenshot();
-    const params = {Bucket: 'ponto', Key: `${dateNow}_ponto.png`, ACL: 'public-read', Body: screenshot};
-    s3.upload(params, (err, data) => console.log(data.Location));
-
-    await page.waitForTimeout(500);
-    console.log("Finaliza");
-    await browser.close();
-};
+bree.start();
 
 const app = express();
-
 app.get('/', function (req, res) {
-    res.send('Hello word!');
+    res.send(`Olá ${process.env.NAME} hoje é ${new Date().toString()}`);
 });
-
 app.listen(3000, () => console.log("Server is running on: localhost:3000"));
 // Mouse: 990 x 578
 // checkPoint();
---
+++ jobs/point.js
@@ -0,0 +1,55 @@
+const pup = require("puppeteer");
+const aws = require("aws-sdk");
+const user = process.env.PONTO_USER;
+const password = process.env.PONTO_PASS;
+
+const s3 = new aws.S3({
+    endpoint: process.env.VULTR_ENDPOINT,
+    accessKeyId: process.env.VULTR_ACCESS_KEY,
+    secretAccessKey: process.env.VULTR_SECRET_KEY,
+    region: process.env.VULTR_REGION
+});
+
+(async () => {
+    const browser = await pup.launch({headless: true});
+    const page = await browser.newPage();
+
+    console.log("Inicia");
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions("https://app2.pontomais.com.br/registrar-ponto", ['geolocation'])
+
+    await page.setGeolocation({latitude:-25.495198, longitude:-49.2415078})
+    await page.setViewport({ width: 1280, height: 650 })
+    await page.goto("https://app2.pontomais.com.br");
+
+    await page.waitForSelector('input[type=text]');
+    await page.type('input[type=text]', user);
+    await page.type('input[type=password]', password);
+
+    console.log("Preenche o login");
+    await page.waitForTimeout(500);
+
+    await Promise.all([
+        page.waitForNavigation(),
+        page.keyboard.press('Enter'),
+    ])
+
+    await page.waitForSelector('.btn-register');
+    console.log("Espera o carregamento do endereço");
+    await page.waitForTimeout(10000);
+
+    console.log("Clica no ponto");
+    // await page.mouse.click(990, 578);
+    await page.mouse.click(1009, 514);
+    await page.waitForTimeout(2000);
+
+    console.log("Gera print do ponto");
+    const dateNow = new Date().getTime();
+    const screenshot = await page.screenshot();
+    const params = {Bucket: 'ponto', Key: `${dateNow}_ponto.png`, ACL: 'public-read', Body: screenshot};
+    s3.upload(params, (err, data) => console.log(data.Location));
+
+    await page.waitForTimeout(500);
+    console.log("Finaliza");
+    await browser.close();
+})();
